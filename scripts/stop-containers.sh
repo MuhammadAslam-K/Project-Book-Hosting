d7e9b41ea2e0591563abc5b168a8dca77652a2a0
MuhammadAslam-K <aslamka.2k3@gmail.com>
2023-11-24 23:18:21 +0530

    updated the script stop-container

--- scripts/stop-containers.sh
+++ scripts/stop-containers.sh
@@ -4,13 +4,7 @@ DEPLOY_DIR="/home/bookat/devopspipeline"
 # Change to the deployment directory
 cd "$DEPLOY_DIR"
 
-# Check if the 'build' directory exists
-if [ -d "server" ]; then
-    # Copy the contents of the 'server' directory to 'nginx'
-    sudo cp -r server/* nginx
-else
-    echo "The 'server' directory does not exist. No copying performed."
-fi
+
 
 # Check if Docker is not running, then start Docker
 if ! docker info > /dev/null 2>&1; then
